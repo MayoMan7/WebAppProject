a4f807b6085418147c6e17dd977255e4d557fa36
Rohin Kumar <76756199+MayoMan7@users.noreply.github.com>
2024-09-26 20:49:17 -0400

    l01

--- util/request.py
+++ util/request.py
@@ -2,14 +2,24 @@ class Request:
 
     def __init__(self, request: bytes):
         # TODO: parse the bytes of the request and populate the following instance variables
-
-        self.body = b""
-        self.method = ""
-        self.path = ""
-        self.http_version = ""
+        lines, self.body = (request.split(b"\r\n\r\n"))
+        lines = lines.split(b"\r\n")
+        self.method = lines[0].split(b" ")[0].decode()
+        self.path = lines[0].split(b" ")[1].decode()
+        self.http_version = lines[0].split(b" ")[2].decode()
         self.headers = {}
         self.cookies = {}
+        for header in lines[1:]: # the second line to the 3rd to last line
+            print(header)
+            if header == b"":
+                continue 
+            if header.split(b":",1)[0].decode() == "Cookie":
+                for cookie in header.split(b":",1)[1].decode().strip().split("; "):
+                    self.cookies[cookie.split("=")[0]] = cookie.split("=")[1]
 
+            # else:
+            self.headers[header.split(b":",1)[0].decode().strip()] = header.split(b":",1)[1].decode().strip()
+        
 
 def test1():
     request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\n')
@@ -24,5 +34,7 @@ def test1():
     # test using a POST request. Also, ensure that the types of all values are correct
 
 
-if __name__ == '__main__':
+# Run the test function
+if __name__ == "__main__":
     test1()
+
--- util/router.py
+++ util/router.py
@@ -1,10 +1,24 @@
 class Router:
 
     def __init__(self):
+        self.routes = []
         pass
 
     def add_route(self, method, path, action, exact_path=False):
+        self.routes.append((method, path, action, exact_path))
         pass
 
     def route_request(self, request, handler):
-        pass
+        for route in self.routes:
+            method, path, action, exact_path = route
+            if request.method == method:
+                if exact_path:
+                    if request.path == path:
+                        return action(request, handler)
+                else:
+                    if request.path.startswith(path):
+                        return action(request, handler) 
+
+        response_404 = ("HTTP/1.1 404 Not Found\r\n""Content-Type: text/plain\r\n""Content-Length: 15\r\n""\r\n""404 Not Found")
+
+        handler.request.sendall(response_404.encode())
